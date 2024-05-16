bab0d0c7bc41427fe4f2ae80a31a8eaa46591b67
Invers3 <manyandamariano@hotmail.com>
2024-05-15 22:43:10 -0500

    Update run.sh
    Reescrito y corregido para mini router

--- run.sh
+++ run.sh
@@ -2,48 +2,27 @@
 
 INTERFACE="br-lan"
 FIRMWAREVERSION="750"
-SHUTDOWN=true
-USBETHERNET=false
+COUNTNUM=0
+RETRYMAX=5
 
-echo -e "\n\n _____  _____  _____"
-echo "|  __ \\|  __ \\|  __ \\"
-echo "| |__) | |__) | |__) |_      ___ __"
-echo "|  ___/|  ___/|  ___/\\ \\ /\\ / / '_ \\"
-echo "| |    | |    | |     \\ V  V /| | | |"
-echo "|_|    |_|    |_|      \\_/\\_/ |_| |_|"
-echo -e "\nhttps://github.com/TheOfficialFloW/PPPwn\n"
-
-if [ "$USBETHERNET" = "true" ]; then
-    echo '1-1' > /sys/bus/usb/drivers/usb/unbind
-    sleep 2
-    echo '1-1' > /sys/bus/usb/drivers/usb/bind
-    sleep 5
-else
-    ifconfig $INTERFACE down
-    sleep 5
-fi
+ifconfig $INTERFACE down
+sleep 5
 ifconfig $INTERFACE up
-
-echo -e "\nReady for console connection\nFirmware: $FIRMWAREVERSION\nInterface: $INTERFACE\n"
 
 while true; do
     ret=$(/root/pppwn --interface $INTERFACE --fw $FIRMWAREVERSION --stage1 /root/stage1_$FIRMWAREVERSION.bin --stage2 /root/stage2_$FIRMWAREVERSION.bin --auto-retry)
+    if [ "$COUNTNUM" -eq "$RETRYMAX" ]; then
+    break
+    fi
     if [ "$ret" -ge 1 ]; then
-        echo -e "\nConsole PPPwned!\n"
-        if [ "$SHUTDOWN" = "true" ]; then
-            poweroff
-        fi
-        exit 1
+        echo "\n$ret\n" > /root/state.txt
+        echo -e "\nConsole PPPwned!\n" >> /root/state.txt
+        break
     else
-        echo -e "\nFailed retrying...\n"
-        if [ "$USBETHERNET" = "true" ]; then
-            echo '1-1' > /sys/bus/usb/drivers/usb/unbind
-            sleep 5
-            echo '1-1' > /sys/bus/usb/drivers/usb/bind
-        else
-            ifconfig $INTERFACE down
-            sleep 5
-            ifconfig $INTERFACE up
-        fi
+        COUNTNUM=$((COUNTNUM+1))
+        echo -e "\nFailed retrying...\n" > /root/state.txt
+        ifconfig $INTERFACE down
+        sleep 5
+        ifconfig $INTERFACE up
     fi
 done
