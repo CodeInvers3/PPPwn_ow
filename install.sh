2e3d0eb1e1c70cc7f50ade82d501448575423fc5
gplayec <netplayec@hotmail.com>
2024-05-18 17:36:56 -0500

    update install.sh
    little correction

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@
 mv -f ~/PPPwn_ow-main/www/pppwn/* /www
 mv -f ~/PPPwn_ow-main/www/pppwn.html /www
 mv -f ~/PPPwn_ow-main/www/cgi-bin/wpwn.cgi /www/cgi-bin
-rm -r ~/PPPwn_ow-main/www
 mv -f ~/PPPwn_ow-main/run.sh ~/
 rm -r PPPwn_ow-main main
 
