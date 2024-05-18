172ee0f192319ef3b968ecbd84ff8be323bcc4ff
gplayec <netplayec@hotmail.com>
2024-05-18 17:46:44 -0500

    Update install.sh
    Correction install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 #!/bin/sh /etc/rc.common
 
-mv -f ~/PPPwn_ow-main/www/pppwn/* /www
+mv -f ~/PPPwn_ow-main/www/pppwn /www
 mv -f ~/PPPwn_ow-main/www/pppwn.html /www
 mv -f ~/PPPwn_ow-main/www/cgi-bin/wpwn.cgi /www/cgi-bin
 mv -f ~/PPPwn_ow-main/run.sh ~/
 rm -r PPPwn_ow-main main
 
 chmod +x /www/cgi-bin/wpwn.cgi
+
+echo "Installation completed!"
