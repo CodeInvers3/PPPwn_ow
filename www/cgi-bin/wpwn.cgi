1b5709c802b40bdbaa0f35b8661bd8e551bd74e6
gplayec <netplayec@hotmail.com>
2024-05-18 01:00:36 -0500

    update www
    add web manager

---
+++ www/cgi-bin/wpwn.cgi
@@ -0,0 +1,102 @@
+#!/bin/sh
+echo "Content-Type: application/json"
+echo ""
+
+adapter="br-lan"
+firmware="1100"
+token="token_id"
+countattempts=0
+signalfile="/www/stop"
+
+read postData
+
+token=$(echo $postData | sed -n 's/^.*token=\([^&]*\).*$/\1/p' | sed "s/%20/ /g")
+adapter=$(echo $postData | sed -n 's/^.*adapter=\([^&]*\).*$/\1/p' | sed "s/%20/ /g")
+firmware=$(echo $postData | sed -n 's/^.*firmware=\([^&]*\).*$/\1/p' | sed "s/%20/ /g")
+path=$(echo $postData | sed -n 's/^.*path=\([^&]*\).*$/\1/p' | sed "s/%20/ /g")
+task=$(echo $postData | sed -n 's/^.*task=\([^&]*\).*$/\1/p' | sed "s/%20/ /g")
+
+if [ "$token" = "token_id" ]; then
+
+    if [ "$task" = "adapters" ]; then
+
+        eths=$(/root/pppwn list)
+        echo "$eths"
+
+    elif [ "$task" = "payloads" ]; then
+
+        payloads=$(ls /root/*.bin)
+        count=0
+        separator=""
+        echo "["
+
+        for payload in $payloads; do
+            if echo "$payload" | grep -q "stage1"; then
+                if [ "$count" -gt "0" ]; then
+                    separator=","
+                fi
+                prts=$(echo $payload | sed -e 's/.*_/ /g' -e 's/\.bin/ /g')
+                echo "$separator\"$prts\""
+            fi
+            count=$((count+1))
+        done
+        echo "]"
+
+    elif [ "$task" = "run" ]; then
+    
+        if [ -f "$signalfile" ]; then
+            rm "$signalfile"
+        fi
+        
+        ip link set $adapter down
+        sleep 5
+        ip link set $adapter up
+        
+        while true; do
+
+            countattempts=$((countattempts+1))
+            pwn=$(/root/pppwn --interface "$adapter" --fw $firmware --stage1 /root/stage1_$firmware.bin --stage2 /root/stage2_$firmware.bin --auto-retry)
+            if [ "$pwn" -ge 1 ]; then
+                echo "Exploit success!" > "/www/pppwn/state.txt"
+                exit 0
+            else
+                echo "Failed! attempting($countattempts)..." > "/www/pppwn/state.txt"
+                ip link set $adapter down
+                sleep 5
+                ip link set $adapter up
+            fi
+            if [ -f "$signalfile" ]; then
+                pids=$(pgrep pppwn)
+                for pid in $pids; do
+                    kill $pid
+                done
+                pids=$(pgrep /root/pppwn)
+                for pid in $pids; do
+                    kill $pid
+                done
+                echo "Termination signal detected. Exiting loop." > "/www/pppwn/state.txt"
+                exit 0
+            fi
+
+        done
+
+    elif [ "$task" = "stop" ]; then
+
+        echo "Stopped!" > "$signalfile"
+
+    elif [ "$task" = "enable" ]; then
+
+        if ! grep -q '/root/run.sh' /etc/rc.local; then
+            sed -i '/exit 0/d' /etc/rc.local
+            echo '/root/run.sh &' >> /etc/rc.local
+            echo 'exit 0' >> /etc/rc.local
+        fi
+        echo "Enabled!"
+
+    fi
+
+else
+
+    echo "Invalid token!"
+
+fi
---
+++ www/pppwn/main.js
@@ -0,0 +1,143 @@
+var appView = Backbone.View.extend({
+    el: '#app-v-pppwn',
+    template: _.template($('#appWeb').html()),
+    initialize: function(){
+        var self = this;
+        var params = new URLSearchParams({
+            task:'adapters',
+            token:'token_id'
+        });
+        fetch('/cgi-bin/wpwn.cgi', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: params.toString(),
+        }).then(function(response){
+            if(response.ok){
+                return response.text();
+            }else{
+                throw new Error("Error cannot execute task.");
+            }
+        })
+        .then(function(data){
+            var interfaces = data.replace(/\[\+\] PPPwn\+\+ - PlayStation 4 PPPoE RCE by theflow|\[\+\] interfaces:|\r| \r/gi, function(value){
+                return "";
+            });
+            self.interfaces(interfaces.split("\n"));
+        })
+        .catch(function(error){
+            $('#task-log').find('.view').append(error+'<br>');
+        });
+        this.data = {
+            interfaces: [],
+            firmware: [
+                {"version":"11.00","value":"1100"},
+                {"version":"10.00","value":"1000"},
+                {"version":"9.00","value":"900"},
+                {"version":"7.50","value":"750"},
+                {"version":"7.00","value":"700"}
+            ]
+        }
+        this.render();
+    },
+    interfaces: function(data){
+        var list = [];
+        data.forEach(function(value){
+            if(value){
+                list.push(value);
+            }
+        });
+        this.data.interfaces = list;
+        this.render();
+    },
+    render: function(){
+        this.$el.html(this.template(this.data));
+        return this;
+    },
+    events: {
+        'click #id_run': function(){
+
+            var path = this.$el.find('[name=path]');
+            var adapter = this.$el.find('[name=adapter]');
+            var firmware = this.$el.find('[name=firmware]');
+
+            var params = new URLSearchParams({
+                task:'run',
+                token:'token_id',
+                path:path.val(),
+                adapter:adapter.val(),
+                firmware:firmware.val()
+            });
+            fetch('/cgi-bin/wpwn.cgi', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                },
+                body: params.toString(),
+            }).then(function(response){
+                if(response.ok){
+                    return response.text();
+                }else{
+                    throw new Error("Error cannot execute task.");
+                }
+            })
+            .then(function(text){
+                $('#task-log').find('.view').append(text).append('<br>');
+            })
+            .catch(function(error){
+                $('#task-log').find('.view').append(error+'<br>');
+            });
+        },
+        'click #id_stop': function(){
+            var params = new URLSearchParams({
+                task:'stop',
+                token:'token_id'
+            });
+            fetch('/cgi-bin/wpwn.cgi', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                },
+                body: params.toString(),
+            }).then(function(response){
+                if(response.ok){
+                    return response.text();
+                }else{
+                    throw new Error("Error cannot execute task.");
+                }
+            })
+            .then(function(text){
+                var output = $('#task-log').find('.view');
+                output.append(text).append("<br>");
+            })
+            .catch(function(error){
+                $('#task-log').find('.view').append(error+'<br>');
+            });
+        },
+        'click #id_enable': function(){
+            var params = new URLSearchParams({
+                task:'enable',
+                token:'token_id'
+            });
+            fetch('/cgi-bin/wpwn.cgi', {
+                method: 'POST',
+                body: params.toString(),
+            }).then(function(response){
+                if(response.ok){
+                    return response.text();
+                }else{
+                    throw new Error("Error cannot execute task.");
+                }
+            })
+            .catch(function(error){
+                $('#task-log').find('.view').append(error+'<br>');
+            })
+            .then(function(text){
+                var output = $('#task-log').find('.view');
+                output.append(text).append("<br>");
+            });
+        }
+    }
+});
+new appView();
