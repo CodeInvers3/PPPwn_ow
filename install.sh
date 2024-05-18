58e250821108c6ce5a09c056d5568b9e0d38a810
gplayec <netplayec@hotmail.com>
2024-05-18 17:51:25 -0500

    Update install.sh
    update again

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 #!/bin/sh /etc/rc.common
 
-chmod +x PPPwn_ow-main/install.sh
-
 mv -f ~/PPPwn_ow-main/www/pppwn /www
 mv -f ~/PPPwn_ow-main/www/pppwn.html /www
 mv -f ~/PPPwn_ow-main/www/cgi-bin/wpwn.cgi /www/cgi-bin
