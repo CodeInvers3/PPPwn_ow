47d40f6e4d67d5b378865d495d6e6227ad093d29
gplayec <netplayec@hotmail.com>
2024-05-24 08:43:49 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 
 INTERFACE=""
 FIRMWARE=""
+COUNTNUM="0"
 
 ip link set $INTERFACE down
 sleep 5
@@ -11,11 +12,11 @@ while true; do
     ret=$(pppwn --interface "$INTERFACE" --fw "$FIRMWARE" --stage1 "/root/offsets/stage1_$FIRMWARE.bin" --stage2 "/root/offsets/stage2_$FIRMWARE.bin" --auto-retry)
     if [ "$ret" -ge 1 ]; then
         echo "\n$ret\n" > /www/pppwn/state.txt
-        echo -e "\nConsole PPPwned!\n" >> /www/pppwn/state.txt
+        echo -e "\nConsole PPPwned!\n" >> /root/state.txt
         exit 0
     else
         COUNTNUM=$((COUNTNUM+1))
-        echo -e "\nFailed retrying...\n" > /www/pppwn/state.txt
+        echo -e "\nFailed ($COUNTNUM) retrying...\n" > /root/state.txt
         ip link set $INTERFACE down
         sleep 5
         ip link set $INTERFACE up
