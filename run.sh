fed43c775774086edf8ff7674e660338ba1c5913
gplayec <netplayec@hotmail.com>
2024-05-24 08:44:38 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -11,7 +11,7 @@ ip link set $INTERFACE up
 while true; do
     ret=$(pppwn --interface "$INTERFACE" --fw "$FIRMWARE" --stage1 "/root/offsets/stage1_$FIRMWARE.bin" --stage2 "/root/offsets/stage2_$FIRMWARE.bin" --auto-retry)
     if [ "$ret" -ge 1 ]; then
-        echo "\n$ret\n" > /www/pppwn/state.txt
+        echo "\n$ret\n" > /root/state.txt
         echo -e "\nConsole PPPwned!\n" >> /root/state.txt
         exit 0
     else
