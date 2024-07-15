a6cb256516929e472a791136f6bee6ae7140946a
Invers3 <manyandamariano@hotmail.com>
2024-07-14 23:43:27 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -12,16 +12,16 @@ if ! pgrep pppwn > /dev/null; then
         
         result=$(pppwn --interface "$interface" --fw "$version" --stage1 "$stage1" --stage2 "$stage2" --timeout $timeout --auto-retry)
         if [[ "$result" == *"\[\+\] Done\!"* ]]; then
-            echo "PPPwn Success"
+            logger "PPPwn Success"
             exit 0
         else
-            echo "PPPwn fail"
+            logger "PPPwn fail"
             exit 1
         fi
 
     fi
 
 else
-    echo "PPPwn is not installed"
+    logger "PPPwn is not installed"
     exit 1
 fi
